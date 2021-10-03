1903ab49ac14657e3547164e95636abf0b735eae
MotoZe <aa8764114@gmail.com>
2021-10-03 17:48:12 +0800

    10/3 C語言練習

---
+++ 3. if_else2/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//巢狀if判斷
+int main()
+{
+    int num = 14;
+    if(num % 7 == 0)
+        printf("%d 可以被7整除\n", num);
+
+        if(num % 7 == 0 && num % 2 == 0)
+            printf("%d 還可以被2整除\n", num);
+
+    else if(num % 7 != 0)
+        printf("滾\n");
+}
